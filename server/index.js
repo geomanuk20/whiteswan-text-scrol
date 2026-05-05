e939b13fc70cc8e705a8adad70b66b2350e572c7
Geo Manu k <geomanuk@Geos-MacBook-Air.local>
2026-05-05 12:28:35 +0530

    fix: express routing error and optimize production start script

--- server/index.js
+++ server/index.js
@@ -127,7 +127,7 @@ app.delete('/api/scrolls/:id', async (req, res) => {
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
